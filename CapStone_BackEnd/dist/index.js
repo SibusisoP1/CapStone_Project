0700276b0e7e1e0549134b8ef5ee9f8dcaa048d8
Sibusiso <218057645@student.uj.ac.za>
2026-07-08 23:37:16 +0200

    fix: support case-sensitive PORT env variable for Render

--- CapStone_BackEnd/dist/index.js
+++ CapStone_BackEnd/dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./server");
 let server = new server_1.Server().app;
-let port = process.env.port || 3000;
+let port = process.env.PORT || process.env.port || 3000;
 server.listen(port, () => {
     console.log(`server is running at port ${port}`);
 });
--- CapStone_BackEnd/src/index.ts
+++ CapStone_BackEnd/src/index.ts
@@ -1,7 +1,7 @@
 import { Server } from "./server";
 
 let server = new Server().app;
-let port = process.env.port || 3000;
+let port = process.env.PORT || process.env.port || 3000;
 
 server.listen(port, () => {
   console.log(`server is running at port ${port}`);
